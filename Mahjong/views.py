f266e9131bc45d5c1a6320e17eb92d8834e483ac
Erik_L <liue@reed.edu>
2023-03-02 22:18:24 -0800

    Update details

--- Mahjong/views.py
+++ Mahjong/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
-
+from django.http import JsonResponse
 # df = getting_data.get_historical_data("AAPL", "2019-01-01", "2020-01-01", "1d")
 
 # def hello(request):
@@ -12,14 +12,9 @@ from django.contrib.auth import authenticate, login
 
 def search_form(request):
     return render(request, 'search_form.html')
-
-def create_room(request):
-    pass
- 
  
 class HomeView(TemplateView):
     template_name = 'home.html'
- 
  
 class CreateUserView(CreateView):
     template_name = 'register.html'
