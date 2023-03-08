72da3601c6862fc69a1b1b85fbbcd6319ebe808a
LouiseX-10 <louisexu426@gmail.com>
2023-03-07 23:36:29 -0800

    Add details to main page

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -5,6 +5,8 @@ import JoinRoomButton from './JoinRoomButton';
 function MainPage() {
     return (
         <div>
+            <h3>Welcome to Reed Mahjong!</h3>
+            <hr></hr>
             <CreateRoomButton />
             <JoinRoomButton />
         </div>
