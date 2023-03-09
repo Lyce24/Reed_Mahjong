57768634ab386fedbc7154afca53796c07f27406
Erik_L <liue@reed.edu>
2023-03-09 00:13:14 -0800

    Creating room api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,15 +4,18 @@ from rest_framework import routers
 from App import views
 import re
 
-router = routers.DefaultRouter()
-router.register(r'todos', views.TodoView, 'todo')
-router.register(r'rooms', views.RoomView, 'room')
+# router = routers.DefaultRouter()
+# router.register(r'todos', views.TodoView, 'todo')
+# router.register(r'rooms', views.RoomView, 'room')
+# router.register(r'students', views.StudentView, 'student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('', views.welcome_page),
-    re_path(r'^api/students/$', views.students_list),
-    re_path(r'^api/students/([0-9])$', views.students_detail),
-    # re_path(r'^api/rooms/[0-9]{8}$', views.create_room),
+    path('', views.welcome_page),    
+    re_path(r'^room/(\d{8})$', views.room_detail),
+    re_path(r'^room/$', views.RoomView.as_view({'get': 'list', 'post': 'create'})),
+    
+    # re_path(r'^api/students/$', views.students_list),
+    # re_path(r'^api/students/([0-9])$', views.students_detail),
+
 ]
