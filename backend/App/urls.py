1bee5ec43e457768fe76c32f95a133c616bf0e2d
Erik_L <liue@reed.edu>
2023-03-21 23:17:52 -0700

    Update structure

---
+++ backend/App/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from rest_framework import routers
+import views
+import re
+
+# router = routers.DefaultRouter()
+# router.register(r'todos', views.TodoView, 'todo')
+# router.register(r'rooms', views.RoomView, 'room')
+# router.register(r'students', views.StudentView, 'student')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.welcome_page),    
+    re_path(r'^room/(\d{8})$', views.room_detail),
+    re_path(r'^room/$', views.RoomView.as_view({'get': 'list', 'post': 'create'})),
+    
+    # re_path(r'^api/students/$', views.students_list),
+    # re_path(r'^api/students/([0-9])$', views.students_detail),
+
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,21 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from rest_framework import routers
-from App import views
-import re
-
-# router = routers.DefaultRouter()
-# router.register(r'todos', views.TodoView, 'todo')
-# router.register(r'rooms', views.RoomView, 'room')
-# router.register(r'students', views.StudentView, 'student')
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.welcome_page),    
-    re_path(r'^room/(\d{8})$', views.room_detail),
-    re_path(r'^room/$', views.RoomView.as_view({'get': 'list', 'post': 'create'})),
-    
-    # re_path(r'^api/students/$', views.students_list),
-    # re_path(r'^api/students/([0-9])$', views.students_detail),
+    # path('admin/',admin.site.urls),
+    path('', include('App.urls')),
+] 
 
-]
