8fccac2ba0499f5f2704a65fdbf14e6493016c24
austinszeng <austinaimhigh@gmail.com>
2023-03-22 14:22:49 -0700

    Point root routing config at App.routing module

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -13,4 +13,15 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import App.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            App.routing.websocket_urlpatterns
+        )
+    ),
+})
