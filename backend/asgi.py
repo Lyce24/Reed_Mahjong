be9b0c89f0bad2766a5a2ad41a8296bfc923d655
austinszeng <austinaimhigh@gmail.com>
2023-03-27 20:47:53 -0700

    Set up stuff for WebSockets

--- backend/asgi.py
+++ backend/asgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+import django
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Only HTTP for now, can add other protocols later
+})
