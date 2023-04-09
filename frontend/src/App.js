2e3468914ecf54e7d51c139223044a69318cd100
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-04-08 22:05:26 -0700

    updated some websocket stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import MainPage from './components/MainPage';
 import { Route, Routes } from 'react-router-dom';
 import RoomPage from './components/RoomPage';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+
+const SOCKET_URL = 'http://localhost:80'
 
 export default function App(prop) {
-    return (
-      <Routes>
-        <Route path='/' element={<MainPage />}/>
-        <Route path='/room/:roomid' element={<RoomPage/>}/>
-      </Routes>
-    );
+  const { sendMessage, lastMessage, readyState } = useWebSocket(SOCKET_URL);
+
+  return (
+    <Routes>
+      <Route path='/' element={<MainPage />} />
+      <Route path='/room/:roomid' element={<RoomPage />} />
+    </Routes>
+  );
 }
