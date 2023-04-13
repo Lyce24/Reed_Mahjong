9bf60e3c57ba118413f298ce5d78975d3c460d35
LouiseX-10 <louisexu426@gmail.com>
2023-04-13 16:15:15 -0700

    Only select one tile at a time

--- frontend/src/components/PlayerBoard.jsx
+++ frontend/src/components/PlayerBoard.jsx
@@ -1,17 +1,39 @@
 import Tile from './Tile';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import '../index.css';
+import DiscardButton from './DiscardButton';
 
 export default function PlayerBoard() {
 
+    let reference = useRef(0);
+
     // 13 tiles + 1 drawn tile
-    let initialTiles = Array(13).fill({
+    let initialTiles = Array(13);
+    for (let i = 0; i<13; i++) {
+        initialTiles.push({
         suite: "bamboo",
-        number: 1
-    })
+        number: Math.min(i+1,9),
+        index: i
+        })
+    }
     const [hand, setHand] = useState(initialTiles);
+    const [selectedTile, setSelectedTile] = useState(null);
+
+    function handleTileClick(index) {
+        if (index === selectedTile) {
+            // If the clicked child is already selected, deselect it
+            setSelectedTile(null);
+        } else {
+            // Otherwise, deselect the previously selected child and select the new child
+            setSelectedTile(index);
+        }
+    }
     //const [drawnTile, setTile] = useState({suite: "", number: 0});
-    
+
+    /* function handleDiscard(params) {
+        console.log(reference.current.state)
+        // collect tile that is clicked
+    } */
     
     // Template for adding elements to array
     /* const temp = () => {
@@ -22,12 +44,13 @@ export default function PlayerBoard() {
         setHand(updateHand);
     }; */
     
-
+//<DiscardButton onClick={handleDiscard}/>
     return (
         <div className='board playerBoard'>
             {hand.map(tile => (
-                <Tile suite={tile.suite} number={tile.number} isFacedDown="false"/>
+                <Tile suite={tile.suite} number={tile.number} index={tile.index} onClick={handleTileClick} isSelected={selectedTile == tile.index} isFacedDown="false"/>
             ))}
         </div>
+        
     );
 }
