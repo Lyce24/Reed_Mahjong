f475ca6898e3486f61f1925be320c30ecab22116
LouiseX-10 <louisexu426@gmail.com>
2023-04-14 15:59:20 -0700

    Test working socket

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,7 +1,43 @@
 import CreateRoomButton from './CreateRoomButton';
 import JoinRoom from './JoinRoom';
+//import WebSocketInstance from './WebSocketInstance';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import { useState, useEffect } from 'react';
 
 export default function MainPage() {
+    /* const WebSocket = () => {
+        const [message, setMessage] = useState('');
+      
+        useEffect(() => {
+          WebSocketInstance.connect();
+          WebSocketInstance.addCallbacks(setMessage);
+          return () => {
+            WebSocketInstance.disconnect();
+          };
+        }, []);
+      
+        return (
+          <div>
+            <p>Last message: {message}</p>
+          </div>
+        );
+      }; */
+
+      const URL = 'ws://localhost:8000/ws/room/000/';
+        var socket = new W3CWebSocket(URL);
+        socket.onopen = () =>{
+            console.log("WebSocket Client Connected");
+            socket.send(JSON.stringify({
+                'type': 'placeholder',
+                'message': 'hi',
+            }))
+        }
+        socket.onmessage = function(e) {
+            if (typeof e.data === 'string') {
+                console.log("Received: '" + e.data + "'");
+            }
+        };
+        
     return (
         <div className='page mainPage'>
             <h3>Welcome to Reed Mahjong!</h3>
