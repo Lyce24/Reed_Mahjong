0b17dbcf0a2df054b2fd6b7debc0bcac7f89d6e1
LouiseX-10 <louisexu426@gmail.com>
2023-04-22 19:00:01 -0700

    import useEffect to joinroom

--- frontend/src/components/JoinRoom.jsx
+++ frontend/src/components/JoinRoom.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSocket } from "./SocketProvider";
 import "../index.css";
 import { useNavigate } from "react-router-dom";
