f60d643e80dc2edad3e9905f92aa3b91a3923de8
LouiseX-10 <louisexu426@gmail.com>
2023-04-23 23:18:09 -0700

    Move room listener to main page

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,15 +1,25 @@
-import CreateRoomButton from './CreateRoomButton';
-import JoinRoom from './JoinRoom';
+import CreateRoomButton from "./CreateRoomButton";
+import JoinRoom from "./JoinRoom";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSocket } from "./SocketProvider";
 
 export default function MainPage() {
-        
-    return (
-        <div className='page mainPage'>
-            <h3>Welcome to Reed Mahjong!</h3>
-            <hr></hr>
-            <CreateRoomButton />
-            <br />
-            <JoinRoom />
-        </div>
-    );
+  const socket = useSocket();
+  const navigate = useNavigate();
+  const [roomNum, setRoomNum] = useState(0);
+
+  useEffect(() => {
+    socket.addRoomListener(setRoomNum, navigate);
+  }, [socket, navigate]);
+
+  return (
+    <div className="page mainPage">
+      <h3>Welcome to Reed Mahjong!</h3>
+      <hr></hr>
+      <CreateRoomButton socket={socket} />
+      <br />
+      <JoinRoom socket={socket} roomNum={roomNum} setRoomNum={setRoomNum} />
+    </div>
+  );
 }
