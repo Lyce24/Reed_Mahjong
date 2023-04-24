416931049cde33f13fdae7228479d9c7981bad0d
LouiseX-10 <louisexu426@gmail.com>
2023-04-23 23:51:21 -0700

    Move functions to gameboard

--- frontend/src/components/GameBoard.jsx
+++ frontend/src/components/GameBoard.jsx
@@ -1,21 +1,121 @@
-import PlayerBoard from './PlayerBoard';
-import OtherBoard from './OtherBoard';
-import MiddleSection from './MiddleSection';
-import '../index.css';
+import PlayerBoard from "./PlayerBoard";
+import OtherBoard from "./OtherBoard";
+import MiddleSection from "./MiddleSection";
+import "../index.css";
+import { useEffect, useState } from "react";
+import { useSocket } from "./SocketProvider";
+import { useUsername } from "./UsernameProvider";
+import { nanoid } from "nanoid";
 
 export default function GameBoard() {
+  const socket = useSocket();
+  const username = useUsername();
 
-    return (
-        <div className='gameBoard'>
-            <PlayerBoard />
-            <br />
-            <OtherBoard orientation="leftBoard" />
-            <br />
-            <OtherBoard orientation="rightBoard" />
-            <br />
-            <OtherBoard orientation="topBoard" />
-            <br />
-            <MiddleSection />
-        </div>
-    );
+  // initialize 13 tiles for player hand, for test purposes only
+  let initialTiles = []; // Array();
+  for (let i = 0; i < 13; i++) {
+    initialTiles.push({
+      suite: "bamboo",
+      number: Math.min(i + 1, 9),
+      index: i,
+      key: nanoid(),
+    });
+  }
+  const [hand, setHand] = useState(null);
+  // setup start tiles listener, get initial 'hand' when receive backend 'start_tiles' msg
+
+  const [selectedTileIndex, setSelectedTileIndex] = useState(null);
+  // select the tile that is clicked
+  function handleTileClick(index) {
+    if (index === selectedTileIndex) {
+      // If the clicked child is already selected, deselect it
+      setSelectedTileIndex(null);
+    } else {
+      // Otherwise, deselect the previously selected child and select the new child
+      setSelectedTileIndex(index);
+    }
+  }
+
+  //TODO: get drawn tile from backend (after backend is implemented)
+  // initialize drawn tile, for test purposes only
+  // change initial value to null after tests
+  const [drawnTile, setDrawnTile] = useState({
+    suite: "bamboo",
+    number: 2,
+    index: 100,
+    key: nanoid(),
+  });
+
+  socket.addStartTileListener(setHand, username);
+  console.log("add player listener");
+  // setup draw listener, updates 'drawnTile' when receive backend 'draw_tile' msg
+  //socket.addDrawListener(setDrawnTile);
+
+  // remove selected tile from hand, submit 'discard_tile' msg to backend
+  // add drawn tile to hand, reorder and reindex hand tiles
+  function handleDiscard(params) {
+    if (selectedTileIndex == null) {
+      //alert("You have not selected any tile!");
+      console.log(hand);
+    } else {
+      //TODO: send discard_tile msg to backend
+      socket.send({
+        type: "discard_tile",
+        tile: hand[selectedTileIndex],
+      });
+      // remove tile that is selected
+      let updatedHand = hand.toSpliced(selectedTileIndex, 1);
+      // put drawn tile in hand
+      updatedHand = [...updatedHand, drawnTile];
+      // set drawn tile to null
+      setDrawnTile(null);
+      // reorder tiles
+      updatedHand.sort(compareTile);
+      // reindex tiles
+      updatedHand.forEach((tile, index) => {
+        tile.index = index;
+      });
+      setHand(updatedHand);
+      setSelectedTileIndex(null);
+    }
+  }
+
+  return (
+    <div className="gameBoard">
+      <PlayerBoard
+        hand={hand}
+        setSelectedTileIndex={setSelectedTileIndex}
+        drawnTile={drawnTile}
+        handleTileClick={handleTileClick}
+        handleDiscard={handleDiscard}
+      />
+      <br />
+      <OtherBoard orientation="leftBoard" />
+      <br />
+      <OtherBoard orientation="rightBoard" />
+      <br />
+      <OtherBoard orientation="topBoard" />
+      <br />
+      <MiddleSection />
+    </div>
+  );
+}
+
+function compareTile(a, b) {
+  // wan < circle < bamboo
+  if (a.suite === b.suite) {
+    return a.number - b.number;
+  } else if (
+    a.suite === "wan" &&
+    (b.suite === "circle" || b.suite === "bamboo")
+  ) {
+    // a before b
+    return -1;
+  } else if (a.suite === "circle" && b.suite === "bamboo") {
+    // a before b
+    return -1;
+  } else {
+    // b before a
+    return 1;
+  }
 }
