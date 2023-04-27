3a9674b912a926ae3465d7a781d58c421707097c
LouiseX-10 <louisexu426@gmail.com>
2023-04-26 20:34:46 -0700

    Test app

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+//import { render, screen } from '@testing-library/react';
+//import App from './App';
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+}); */
+
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { UsernameProvider } from './components/UsernameProvider';
+import { SocketProvider } from './components/SocketProvider';
+import App from './App';
+
+describe('App component', () => {
+  it('renders without crashing', () => {
+    render(
+      <UsernameProvider>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
+      </UsernameProvider>
+    );
+  });
 });
