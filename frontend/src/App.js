16f45631d7452172aca10c8b3ee8892dfe5d4ef6
LouiseX-10 <louisexu426@gmail.com>
2023-04-27 21:26:00 -0700

    Place context providers in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
 import GameRoutes from './routes.js';
+import { SocketProvider } from './components/SocketProvider'
+import { UsernameProvider } from './components/UsernameProvider';
 
 export default function App() {
     return (
-      <BrowserRouter>
-        <GameRoutes/>
-      </BrowserRouter>
+      <UsernameProvider>
+        <SocketProvider>
+          <BrowserRouter>
+            <GameRoutes/>
+          </BrowserRouter>
+        </SocketProvider>
+    </UsernameProvider>
+
     );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import { SocketProvider } from './components/SocketProvider'
+
 import App from './App';
-import { UsernameProvider } from './components/UsernameProvider';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <UsernameProvider>
-        <SocketProvider>
-            <App/> 
-        </SocketProvider>
-    </UsernameProvider>
+    <App/> 
 );
