f052c4f6897553e4a52d9b7426d994ff20b9cf9a
LouiseX-10 <louisexu426@gmail.com>
2023-05-04 17:34:49 -0700

    Clean up unwanted imports

--- frontend/src/components/JoinRoom.jsx
+++ frontend/src/components/JoinRoom.jsx
@@ -1,18 +1,6 @@
-import { useEffect, useState } from "react";
-import { useSocket } from "./SocketProvider";
 import "../index.css";
-import { useNavigate } from "react-router-dom";
 
 export default function JoinRoom(props) {
-  // const [roomNum, setRoomNum] = useState(0);
-  //const socket = useSocket();
-  // const navigate = useNavigate();
-
-  // Set up room listener upon initial render
-  /* useEffect(() => {
-    socket.addRoomListener(setRoomNum, navigate);
-  }, [socket, navigate]); */
-
   // Redirect to room when user clicks button if roomNum is valid
   function handleSubmit(e) {
     e.preventDefault(); // prevent form submission
